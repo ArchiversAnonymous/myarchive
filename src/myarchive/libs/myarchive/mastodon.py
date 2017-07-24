d5f608f853c53d221da892a85f4b9656c1cd82ca
Zeta Syanthis <zeta@zetasyanthis.org>
2017-07-23 19:01:16 -0700

    No DB saving yet, but we hit the API at least.

--- src/myarchive/libs/myarchive/mastodon.py
+++ src/myarchive/libs/myarchive/mastodon.py
@@ -8,16 +8,16 @@ from myarchive.libs.mastodon import Mastodon
 from myarchive.util.lib import CONFIG_FOLDER
 
 LOGGER = logging.getLogger(__name__)
-SECRET_PATH = os.path.join(CONFIG_FOLDER, "mastodon.secret")
+APP_SECRET_PATH = os.path.join(CONFIG_FOLDER, "mastodon.app.secret")
 
 
 def download_toots(db_session, media_storage_path, config):
 
-    if not os.path.exists(SECRET_PATH):
+    if not os.path.exists(APP_SECRET_PATH):
         Mastodon.create_app(
              'myarchive',
              api_base_url='awoo.space',
-             to_file=SECRET_PATH
+             to_file=APP_SECRET_PATH
         )
 
     for config_section in config.sections():
@@ -31,15 +31,27 @@ def download_toots(db_session, media_storage_path, config):
 
             LOGGER.critical([host, username, password])
 
-            mastodon = Mastodon(
-                client_id=SECRET_PATH,
+            mastodon_api = Mastodon(
+                client_id=APP_SECRET_PATH,
                 api_base_url=host,
             )
-            mastodon.log_in(
+            mastodon_api.log_in(
                 username,
                 password,
-                to_file=SECRET_PATH,
             )
+            user = mastodon_api.account_verify_credentials()
 
-            # mastodon.toot(
-            #     status="OMG IT WORKS!")
+            toots = []
+
+            results_page = mastodon_api.account_statuses(
+                id=user["id"], max_id=None, since_id=None, limit=None)
+            toots.extend(results_page)
+            while results_page:
+                results_page = mastodon_api.fetch_next(
+                    previous_page=results_page)
+                if results_page is not None:
+                    toots.extend(results_page)
+            LOGGER.info(len(toots))
+
+            # results_page = mastodon.favourites(
+            #     max_id=None, since_id=None, limit=None)
